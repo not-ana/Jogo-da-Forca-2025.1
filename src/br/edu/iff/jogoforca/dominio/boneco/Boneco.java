3f8684e142e0bed09fdb61207a389503426988ab
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2025-09-23 01:05:54 -0300

    Interface Boneco

--- src/br/edu/iff/jogoforca/dominio/boneco/Boneco.java
+++ src/br/edu/iff/jogoforca/dominio/boneco/Boneco.java
@@ -1,5 +1,16 @@
 package br.edu.iff.jogoforca.dominio.boneco;
 
-public class Boneco {
-
+/**
+ * Interface para representar o boneco da forca
+ * Utiliza padrão Strategy para diferentes tipos de exibição
+ */
+public interface Boneco {
+    
+    /**
+     * Exibe o boneco com contexto específico
+     * Implementa o padrão Strategy
+     * @param contexto contexto para exibição
+     * @param partes número de partes do boneco a serem exibidas
+     */
+    void exibir(Object contexto, int partes);
 }
