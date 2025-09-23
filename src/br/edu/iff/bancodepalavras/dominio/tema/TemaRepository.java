d790c576f992324b5d451ee4f534ef109ac44225
siqueiralayla <99771364+siqueiralayla@users.noreply.github.com>
2025-09-23 19:17:33 -0300

    Update TemaRepository.java

--- src/br/edu/iff/bancodepalavras/dominio/tema/TemaRepository.java
+++ src/br/edu/iff/bancodepalavras/dominio/tema/TemaRepository.java
@@ -1,5 +1,21 @@
 package br.edu.iff.bancodepalavras.dominio.tema;
 
-public class TemaRepository {
+import br.edu.iff.repository.Repository;
+import br.edu.iff.repository.RepositoryException;
+
+public interface TemaRepository extends Repository {
+
+  Tema getPorId(long id);
+
+     Tema[] getPorNome(String nome);
+
+     Tema[] getTodos();
+
+     void inserir(Tema tema) throws RepositoryException;
+
+     void atualizar(Tema tema) throws RepositoryException;
+
+     void remover(Tema tema) throws RepositoryException;
+
 
 }
