546dc5032259d9805f7c3d0230d4e93f9b51cbf3
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2025-09-23 00:57:26 -0300

    Commit LetraTextoFactory

--- src/br/edu/iff/bancodepalavras/dominio/letra/texto/LetraTextoFactory.java
+++ src/br/edu/iff/bancodepalavras/dominio/letra/texto/LetraTextoFactory.java
@@ -1,5 +1,41 @@
 package br.edu.iff.bancodepalavras.dominio.letra.texto;
 
-public class LetraTextoFactory {
+import br.edu.iff.bancodepalavras.dominio.letra.Letra;
+import br.edu.iff.bancodepalavras.dominio.letra.LetraFactoryImpl;
 
+/**
+ * Factory concreta para criação de letras em modo texto
+ * Implementa SINGLETON + herda FLYWEIGHT da classe abstrata
+ * Estende LetraFactoryImpl com Template Method pattern
+ */
+public class LetraTextoFactory extends LetraFactoryImpl {
+    
+    // SINGLETON: única instância
+    private static LetraTextoFactory soleInstance;
+    
+    /**
+     * Construtor público conforme diagrama UML
+     */
+    public LetraTextoFactory() {
+        super();
+    }
+    
+    /**
+     * SINGLETON: Método para obter a única instância
+     */
+    public static LetraTextoFactory getSoleInstance() {
+        if (soleInstance == null) {
+            soleInstance = new LetraTextoFactory();
+        }
+        return soleInstance;
+    }
+    
+    /**
+     * TEMPLATE METHOD: Implementação concreta para criar letra texto
+     * Chamado pela classe abstrata quando letra não está no pool
+     */
+    @Override
+    protected Letra criarLetra(char codigo) {
+        return LetraTexto.getFlyweight(codigo);
+    }
 }
