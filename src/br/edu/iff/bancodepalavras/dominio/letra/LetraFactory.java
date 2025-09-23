19990647dc1556951750473b8efe46b8b1b50ecb
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2025-09-23 00:58:28 -0300

    interface LetraFactory

--- src/br/edu/iff/bancodepalavras/dominio/letra/LetraFactory.java
+++ src/br/edu/iff/bancodepalavras/dominio/letra/LetraFactory.java
@@ -1,5 +1,20 @@
 package br.edu.iff.bancodepalavras.dominio.letra;
 
-public class LetraFactory {
-
+/**
+ * Factory interface para criação de letras
+ * Implementa o padrão Abstract Factory conforme diagrama UML
+ */
+public interface LetraFactory {
+    
+    /**
+     * Obtém uma letra com o código especificado
+     * Usa FLYWEIGHT para reutilizar instâncias
+     */
+    public Letra getLetra(char codigo);
+    
+    /**
+     * Obtém uma letra especial para posições encobertas
+     * Retorna sempre a mesma instância (ex: '_')
+     */
+    public Letra getLetraEncoberta();
 }
