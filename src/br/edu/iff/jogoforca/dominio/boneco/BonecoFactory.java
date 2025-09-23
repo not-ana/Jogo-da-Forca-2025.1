2b5453c52d93fa0975c0f61f86b744a1015ff782
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2025-09-23 01:06:18 -0300

    BonecoFactory

--- src/br/edu/iff/jogoforca/dominio/boneco/BonecoFactory.java
+++ src/br/edu/iff/jogoforca/dominio/boneco/BonecoFactory.java
@@ -1,5 +1,13 @@
 package br.edu.iff.jogoforca.dominio.boneco;
 
-public class BonecoFactory {
-
+/**
+ * Factory interface para criação de bonecos
+ * Implementa o padrão Abstract Factory
+ */
+public interface BonecoFactory {
+    
+    /**
+     * Cria um boneco
+     */
+    public Boneco getBoneco();
 }
