dd7bda1fb3836e38336eab1829981f7846551684
siqueiralayla <99771364+siqueiralayla@users.noreply.github.com>
2025-09-24 20:13:45 -0300

    Update JogadorRepository.java

--- src/br/edu/iff/jogoforca/dominio/jogador/JogadorRepository.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/JogadorRepository.java
@@ -1,5 +1,48 @@
 package br.edu.iff.jogoforca.dominio.jogador;
 
-public class JogadorRepository {
+import br.edu.iff.factory.EntityFactory;
+
+
+public class JogadorFactoryImpl extends EntityFactory implements JogadorFactory {
+
+    private static JogadorFactoryImpl soleInstance;
+
+
+    public static void createSoleInstance(JogadorRepository repository) {
+
+        if (soleInstance == null) {
+
+            soleInstance = new JogadorFactoryImpl(repository);
+
+        }
+    }
+
+    public static JogadorFactoryImpl getSoleInstance(){
+
+        if(soleInstance != null) {
+
+            return soleInstance;
+        } else {
+
+            throw new IllegalStateException("A fábrica de jogador não foi definida.");
+        }
+
+    }
+
+    private JogadorFactoryImpl(JogadorRepository jogadorRepository) {
+        super(jogadorRepository);
+
+    }
+
+    private JogadorRepository getJogadorRepository() {
+
+        return (JogadorRepository) getRepository();
+    }
+
+    public Jogador getJogador(String nome) {
+
+        return Jogador.criar(getJogadorRepository().getProximoId(), nome);
+    }
+
 
 }
