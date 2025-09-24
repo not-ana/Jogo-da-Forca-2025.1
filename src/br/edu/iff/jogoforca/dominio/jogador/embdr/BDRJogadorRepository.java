e54e4aea6fe38c183fd3ded5d57b2fbbe32c3d22
siqueiralayla <99771364+siqueiralayla@users.noreply.github.com>
2025-09-24 20:16:20 -0300

    Update BDRJogadorRepository.java

--- src/br/edu/iff/jogoforca/dominio/jogador/embdr/BDRJogadorRepository.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/embdr/BDRJogadorRepository.java
@@ -1,5 +1,55 @@
 package br.edu.iff.jogoforca.dominio.jogador.embdr;
 
-public class BDRJogadorRepository {
+import br.edu.iff.jogoforca.dominio.jogador.Jogador;
+import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+import br.edu.iff.repository.RepositoryException;
 
+public class BDRJogadorRepository implements JogadorRepository {
+
+    private static BDRJogadorRepository soleInstance;
+
+    private BDRJogadorRepository() {}
+
+    public static BDRJogadorRepository getSoleInstance() {
+
+        if (soleInstance == null) {
+
+            soleInstance = new BDRJogadorRepository();
+        }
+
+        return soleInstance;
+    }
+
+    @Override
+    public Jogador getPorId(long id) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'getPorId'");
+    }
+
+    @Override
+    public Jogador getPorNome(String nome) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'getPorNome'");
+    }
+
+    @Override
+    public void inserir(Jogador jogador) throws RepositoryException {
+
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+    }
+
+    @Override
+    public void atualizar(Jogador jogador) throws RepositoryException {
+
+    }
+
+    @Override
+    public void remover(Jogador jogador) throws RepositoryException {
+
+    }
+
+    @Override
+    public long getProximoId() {
+        return 0;
+    }
 }
