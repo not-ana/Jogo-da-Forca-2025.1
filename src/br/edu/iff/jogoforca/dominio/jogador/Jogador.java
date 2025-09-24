45aa8c93fdf5b0353258cec3b0c0641f55e72128
siqueiralayla <99771364+siqueiralayla@users.noreply.github.com>
2025-09-24 20:08:55 -0300

    Update Jogador.java

--- src/br/edu/iff/jogoforca/dominio/jogador/Jogador.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/Jogador.java
@@ -1,5 +1,50 @@
 package br.edu.iff.jogoforca.dominio.jogador;
 
-public class Jogador {
+import br.edu.iff.dominio.ObjetoDominioImpl;
 
+
+public class Jogador extends ObjetoDominioImpl {
+
+	private String nome;
+	private int pontuacao;
+
+	public static Jogador criar(long id, String nome) {
+
+		return new Jogador(id, nome);
+	}
+
+	public static Jogador reconstituir(long id, String nome, int pontuacao) {
+
+		return new Jogador(id, nome, pontuacao);
+	}
+
+	private Jogador(long id, String nome) {
+
+		super(id);
+		this.nome = nome;
+	}
+
+	private Jogador(long id, String nome, int pontuacao) {
+
+		super(id);
+		this.nome = nome;
+		this.pontuacao = pontuacao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+	public void atualizarPontuacao(int pontuacao) {
+
+		this.pontuacao = pontuacao;
+	}
 }
