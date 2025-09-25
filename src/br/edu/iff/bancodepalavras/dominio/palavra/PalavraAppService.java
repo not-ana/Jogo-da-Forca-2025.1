37008d8a4e59f9c036305cc68eea3832c7128d48
Nathalia Carvalho <160883127+Nath-Carvalho@users.noreply.github.com>
2025-09-25 10:29:34 -0300

    Update PalavraAppService.java

--- src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
+++ src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
@@ -1,5 +1,62 @@
 package br.edu.iff.bancodepalavras.dominio.palavra;
 
-public class PalavraAppService {
+import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
+import br.edu.iff.repository.RepositoryException;
 
+public class PalavraAppService {
+    
+    private static PalavraAppService soleInstance;
+    private PalavraFactory palavraFactory;
+    private PalavraRepository palavraRepository;
+    private TemaRepository temaRepository;
+
+
+    private PalavraAppService(TemaRepository temaRepository, PalavraRepository palavraRepository, PalavraFactory factory) {
+        
+        this.palavraFactory = factory;
+        this.palavraRepository = palavraRepository;
+        this.temaRepository = temaRepository;
+    }
+
+
+    public static void createSoleInstance(TemaRepository temaRepository, PalavraRepository palavraRepository, PalavraFactory factory) {
+      
+        soleInstance = new PalavraAppService(temaRepository, palavraRepository, factory);
+    }
+
+    public static PalavraAppService getSoleInstance() {
+
+        if(soleInstance == null) {
+
+            throw new IllegalStateException("A instância de PalavraAppService não foi criada ainda.");
+        }
+
+        return soleInstance;
+    }
+
+    public boolean novaPalavra(String palavra, Long idTema) {
+        
+        if(temaRepository.getPorId(idTema) == null) {
+
+            throw new RuntimeException("Tema não existe no repositório.");
+        }
+
+        if(palavraRepository.getPalavra(palavra) != null) {
+
+            return true;
+        }
+
+        try {
+            
+            palavraRepository.inserir(palavraFactory.criarPalavra(palavra, temaRepository.getPorId(idTema)));
+           
+            return true;
+
+        } catch (RepositoryException e) {
+            
+            System.err.println(e.getMessage() + "Erro ao inserir palavra no Repositório.");
+
+            return false;
+        }
+    }
 }
