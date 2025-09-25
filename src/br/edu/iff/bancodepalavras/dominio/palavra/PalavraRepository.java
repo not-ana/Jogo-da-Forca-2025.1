48a04255cf50bc4eed29255bba96ef0e79566b64
Nathalia Carvalho <160883127+Nath-Carvalho@users.noreply.github.com>
2025-09-25 10:31:21 -0300

    Update PalavraRepository.java

--- src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraRepository.java
+++ src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraRepository.java
@@ -1,5 +1,16 @@
 package br.edu.iff.bancodepalavras.dominio.palavra;
 
-public class PalavraRepository {
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
+import br.edu.iff.repository.Repository;
+import br.edu.iff.repository.RepositoryException;
 
+public interface PalavraRepository extends Repository{
+
+    public Palavra getPorId(long id);
+    public Palavra[] getPorTema(Tema tema);
+    public Palavra[] getTodas(); 
+    public Palavra getPalavra(String palavra);
+    public void inserir(Palavra palavra) throws RepositoryException;
+    public void atualizar(Palavra palavra) throws RepositoryException;
+    public void remover(Palavra palavra) throws RepositoryException;
 }
